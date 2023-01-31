24ee81aec3b6a6355289cd0154c4a9625fbc12a2
NinaT <tietje.hh@gmail.com>
2023-01-31 17:14:42 +0100

    adjusted interactable scripts, added collider to player

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,22 +1,49 @@
+
 using UnityEngine;
 using UnityEngine.Events;
 
+public enum InteractionType
+{
+    Interact,
+    PickUp,
+    Talk
+}
+
 public class Interactable : MonoBehaviour
 {
+    public InteractionType interactionType;
     public bool inRange;
     public UnityEvent interactAction;
     public KeyCode interactKey = KeyCode.E;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
+        if (inRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                Debug.Log("Interaction was called");
+                interactAction.Invoke();
+            }
+        }
+    }
 
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player is in range");
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player is not anymore in range");
+            inRange = false;
+        }
     }
 }
