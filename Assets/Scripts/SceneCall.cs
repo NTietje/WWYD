35d7a13c3743a2f97e8fe36cc512075ade551149
NinaT <tietje.hh@gmail.com>
2023-03-27 16:19:35 +0200

    fixed wasteland animation

--- Assets/Scripts/SceneCall.cs
+++ Assets/Scripts/SceneCall.cs
@@ -8,7 +8,7 @@ public class SceneCall : MonoBehaviour
 {
     [SerializeField] private string sceneName;
     [SerializeField] private int delay;
-    [SerializeField] private Animator animator;
+    [SerializeField] private Animator animator = null;
     [SerializeField] private Animator animator2 = null;
 
     public void StartScene()
@@ -19,21 +19,23 @@ public class SceneCall : MonoBehaviour
 
     public void StartAnimatorAndScene()
     {
+        Debug.Log("start animation");
+        if (animator != null)
+        {
+            Debug.Log("start animation 1");
+            animator.SetBool("enable", true);
+        }
+      
+        if (animator2 != null)
+        {
+            animator2.SetBool("enable", true);
+        }
+        
         StartCoroutine(HandleAnimationAndSceneSwitch());
     }
 
     private IEnumerator HandleAnimationAndSceneSwitch()
     {
-        Debug.Log("start animation");
-        if (animator != null)
-        {
-            animator.SetBool("enable", true);
-        }
-        if (animator2 != null)
-        {
-            animator2.SetBool("enable", true);
-        }
-
         if (sceneName != null)
         {
             Debug.Log("waiting");
